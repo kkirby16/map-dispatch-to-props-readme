6446d2f6f85fceef2f0d0612de152517dafd4ef9
kkirby16 <kkirby16@my.whitworth.edu>
2021-08-25 01:36:51 -0700

    commit tree 4ed679ab1fd55af45d28a44f325daa2381061519

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo } from "./actions/todos";
+import "./App.css";
 
 class App extends Component {
-
   state = {
-    todo: ''
-  }
+    todo: "",
+  };
 
-  handleOnChange = event => {
+  handleOnChange = (event) => {
     this.setState({
-      todo: event.target.value
+      todo: event.target.value,
     });
-  }
+  };
 
-  handleOnSubmit = event => {
+  handleOnSubmit = (event) => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
-    this.setState({ todo: '' });
-  }
+    this.props.addTodo(this.state.todo);
+    this.setState({ todo: "" });
+  };
 
   render() {
-    const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
+    const renderTodos = () =>
+      this.props.todos.map((todo) => <li key={todo}>{todo}</li>);
     return (
       <div className="App">
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
-        <input
-          type="text"
-          onChange={(event) => this.handleOnChange(event)}
-          id="todos"
-          placeholder="add todo" 
-          value={this.state.todo}/>
-        <input type="submit" />
-      </form>
-      <h2>Todos:</h2>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnChange(event)}
+            id="todos"
+            placeholder="add todo"
+            value={this.state.todo}
+          />
+          <input type="submit" />
+        </form>
+        <h2>Todos:</h2>
         <ol>{renderTodos()}</ol>
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default connect((state) => ({ todos: state.todos }), { addTodo })(App);
